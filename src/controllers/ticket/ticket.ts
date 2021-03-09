78f3dd4f9d2b82e0eb1158d48017b39dbf62a4e8
Oluwaseun Adebambo <emma.bambo@yahoo.com>
2021-03-09 09:23:32 +0100

    Update

--- src/controllers/ticket/ticket.ts
+++ src/controllers/ticket/ticket.ts
@@ -18,42 +18,42 @@ export class Ticket {
     }
   }
 
-  public async addTicket(ctx: Context): Promise<void> {
-    try {
-      const body: ITicket = ctx.request.body;
-      const schema = Joi.object().keys({
-        fullname: Joi.string().required(),
-        email: Joi.string().required(),
-        subject: Joi.string().required(),
-        description: Joi.string().required(),
-        department: Joi.string().required(),
-        priority: Joi.string().required(),
-      });
-      const value: ITicket = await schema.validateAsync(body);
-      const { id } = ctx.state.user;
+  // public async addTicket(ctx: Context): Promise<void> {
+  //   try {
+  //     const body: ITicket = ctx.request.body;
+  //     const schema = Joi.object().keys({
+  //       fullname: Joi.string().required(),
+  //       email: Joi.string().required(),
+  //       subject: Joi.string().required(),
+  //       description: Joi.string().required(),
+  //       department: Joi.string().required(),
+  //       priority: Joi.string().required(),
+  //     });
+  //     const value: ITicket = await schema.validateAsync(body);
+  //     const { id } = ctx.state.user;
 
-      value.user = id;
-      value.ticketId = `${Math.floor(Math.random() * RANDOM_VALUE_MULTIPLIER)}`;
-      const ticket = await TicketModel.create(value);
-      if (ticket) {
-        await UserModel.updateOne(
-          {
-            _id: id
-          },
-          {
-            $push: {
-              tickets: {
-                ticket: ticket._id
-              }
-            }
-          }
-        );
-        ctx.body = { message: 'Ticket added successfully', ticket };
-      }
-    } catch (error) {
-      ctx.body = error;
-    }
-  }
+  //     value.user = id;
+  //     value.ticketId = `${Math.floor(Math.random() * RANDOM_VALUE_MULTIPLIER)}`;
+  //     const ticket = await TicketModel.create(value);
+  //     if (ticket) {
+  //       await UserModel.updateOne(
+  //         {
+  //           _id: id
+  //         },
+  //         {
+  //           $push: {
+  //             tickets: {
+  //               ticket: ticket._id
+  //             }
+  //           }
+  //         }
+  //       );
+  //       ctx.body = { message: 'Ticket added successfully', ticket };
+  //     }
+  //   } catch (error) {
+  //     ctx.body = error;
+  //   }
+  // }
 
   public async editTicket(ctx: Context): Promise<void> {
     try {

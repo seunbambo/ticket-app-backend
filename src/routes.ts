8477264f8404499635e391d8c9388d2006f1ae5a
Oluwaseun Adebambo <emma.bambo@yahoo.com>
2021-03-10 11:42:50 +0100

    Update

--- src/routes.ts
+++ src/routes.ts
@@ -19,10 +19,10 @@ export function registerRoutes() {
 
   // TICKET ROUTES
   router.get('/tickets', verifyToken, Ticket.prototype.getAllTickets);
-  // router.post('/tickets/add', verifyToken, Ticket.prototype.addTicket);
+  router.post('/tickets/add', verifyToken, Ticket.prototype.addTicket);
   router.put('/tickets/:id', verifyToken, Ticket.prototype.editTicket);
   router.put('/tickets/mark-ticket/:_id', verifyToken, Ticket.prototype.closeTicket);
-  // router.delete('/tickets/:_id', verifyToken, Ticket.prototype.deleteTicket);
+  router.delete('/tickets/:_id', verifyToken, Ticket.prototype.deleteTicket);
 
   return router;
 }
